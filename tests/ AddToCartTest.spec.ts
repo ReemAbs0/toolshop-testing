d69b5199743a9af766e469637a2ad1674beaeee1
Reem Abs <ReemAbs@users-MacBook-Pro.local>
2025-12-21 19:28:00 +0200

    delete cartPOM

--- tests/ AddToCartTest.spec.ts
+++ tests/ AddToCartTest.spec.ts
@@ -10,14 +10,12 @@ for (const product of products) {
     test('Add to Cart Test for ' + product.name, async ({ page }) => {
 
         const cartPOM = new CartPOM(page);
-
         await page.goto(process.env.BASE_URL!);
 
         await page.locator(product.url).click();
 
         await cartPOM.addProductToCart();
-        await cartPOM.goToCheckout("//span[@data-test='cart-quantity']");
-
+        await cartPOM.goToCheckout(process.env.BASE_URL!);
 
         await page.goto(process.env.BASE_URL!);
 
