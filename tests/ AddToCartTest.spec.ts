3d383699ab71ffafc6013bb68aa886260e15a8f8
Reem Abs <ReemAbs@users-MacBook-Pro.local>
2025-12-21 20:01:17 +0200

    adding changes for add to cart

--- tests/ AddToCartTest.spec.ts
+++ tests/ AddToCartTest.spec.ts
@@ -2,22 +2,28 @@ import { test, expect } from '@playwright/test';
 import { products } from '../pages/Products';
 import { CartPOM } from '../pages/CartPOM';
 import * as dotenv from 'dotenv';
-
 dotenv.config();
 
-for (const product of products) {
+test.describe('Adding To cart', () => {
+    let cart: CartPOM;
 
-    test('Add to Cart Test for ' + product.name, async ({ page }) => {
-
-        const cartPOM = new CartPOM(page);
+    test.beforeEach(async ({ page }) => {
+        cart = new CartPOM(page);
         await page.goto(process.env.BASE_URL!);
-
-        await page.locator(product.url).click();
-
-        await cartPOM.addProductToCart();
-        await cartPOM.goToCheckout(process.env.BASE_URL!);
-
-        await page.goto(process.env.BASE_URL!);
-
     });
-}
+    for (const product of products) {
+
+        test('Add to Cart Test for ' + product.name, async ({ page }) => {
+
+            await page.goto(process.env.BASE_URL!);
+
+            await page.locator(product.url).click();
+
+            await cart.addProductToCart();
+            await cart.goToCheckout(process.env.BASE_URL!);
+
+            await page.goto(process.env.BASE_URL!);
+
+        });
+    }
+});
