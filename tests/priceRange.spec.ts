46cbec9cc7beecf957236aba5db438e91d73a9d5
Reem Abs <ReemAbs@users-MacBook-Pro.local>
2025-12-29 10:40:20 +0200

    deleting unneded code

--- tests/priceRange.spec.ts
+++ tests/priceRange.spec.ts
@@ -27,29 +27,3 @@ test.describe('Price Range', () => {
     await priceRange.verifyPricesInRange(appliedMin, appliedMax);
   });
 });
-/*import { test } from '@playwright/test';
-import { PriceRangePOM } from '../pages/PriceRangePOM';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-const ranges = [
-  [70, 170],
-  [30, 120],
-  [100, 200],
-];
-
-test.describe('Price Range', () => {
-  for (const [min, max] of ranges) {
-    test(`Products respect price range from ${min} to ${max}`, async ({ page }) => {
-      const priceRange = new PriceRangePOM(page);
-
-      await priceRange.openPage(process.env.BASE_URL!);
-
-      const { appliedMin, appliedMax } =
-        await priceRange.applyPriceRange(min, max);
-
-      await priceRange.verifyPricesInRange(appliedMin, appliedMax);
-    });
-  }
-});*/ 
